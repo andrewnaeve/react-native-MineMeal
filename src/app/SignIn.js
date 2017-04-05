3c4daf585a1f2c900709a0761797cb2d341fd533
andrewnaeve <andrewnaeve@gmail.com>
2017-04-04 22:09:48 -0700

    keyboard animation

--- src/app/SignIn.js
+++ src/app/SignIn.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, Keyboard, Text, TextInput, View, Image, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
+import { StyleSheet,
+          Keyboard,  
+          Text, TextInput, 
+          View, 
+          Image, 
+          TouchableOpacity, 
+          TouchableWithoutFeedback,
+          KeyboardAvoidingView, 
+          LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { Components } from 'expo';
 import { height, width, containerWidth, block, RStyles } from './assets/styles/style'; 
 const { LinearGradient } = Components;
+
+
+
+
 
 export default class SignIn extends Component {
 
@@ -13,11 +25,40 @@ export default class SignIn extends Component {
       email: '',
       password: '',
       passwordCheck: '',
+      visibleHeight: height,
+      topLogo: {height: 300, width: 300},
     })
     this.changeEmail = this.changeEmail.bind(this)
     this.changePassword = this.changePassword.bind(this)
     this.checkPassword = this.checkPassword.bind(this)
     this.handleSignUp = this.handleSignUp.bind(this)
+  }
+
+  componentWillMount () {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow.bind(this))
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide.bind(this))
+  }
+
+  componentWillUnmount () {
+    this.keyboardDidShowListener.remove()
+    this.keyboardDidHideListener.remove()
+  }
+
+  keyboardDidShow (e) {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+    let newSize = height - e.endCoordinates.height
+    this.setState({
+      visibleHeight: newSize,
+      topLogo: { width: 200, height: 200 }
+    })
+  }
+
+  keyboardDidHide(e) {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+    this.setState({
+      visibleHeight: height,
+      topLogo: { width: 300, height: 300 }
+    })
   }
 
   changeEmail(text) {
@@ -43,14 +84,15 @@ export default class SignIn extends Component {
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <LinearGradient style={styles.container} colors={['#ffffff', '#04bff3']}>
-          <Image style={styles.logo} source={require("./assets/img/mine_final.png")}/>
+        <LinearGradient style={[styles.container, {height: this.state.visibleHeight}]} colors={['#ffffff', '#04bff3']}>
+          <Image style={[styles.logo, this.state.topLogo]} source={require("./assets/img/mine_final.png")}/>
           <View>
+          
+          <View style={styles.form}>
             <View style={styles.wrap}>
-              <TextInput style={styles.input} placeholder="Email" onChangeText={(text) => this.changeEmail(text)}/>
+              <TextInput style={styles.input} type={'email-address'} placeholder="Email" onChangeText={(text) => this.changeEmail(text)}/>
             </View>
 
-              <View>
                 <View style={styles.wrap}>
                   <TextInput style={[styles.input, 
                     this.state.password === this.state.passwordCheck ? null : styles.red]} 
@@ -77,6 +119,7 @@ export default class SignIn extends Component {
               </Text>
             </LinearGradient>
           </TouchableOpacity>
+          
         </LinearGradient>
       </TouchableWithoutFeedback>
     )
@@ -86,11 +129,10 @@ export default class SignIn extends Component {
 const styles = {
   container: {
     flex: 1,
+    height: height,
     alignItems: 'center',       
   },
   logo: {
-    width: 350,
-    height: 350,
     resizeMode: 'cover',
   },
   input: {
